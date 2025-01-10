30c3ec3b3de57a6e40b4b04dc3f4fb2deffded06
Woong99 <dndry1072@naver.com>
2025-01-10 22:36:51 +0900

    feat: 라이브러리 추가
    1. JPA
    2. JWT
    3. Spring Boot Web
    4. Spring Security
    5. P6Spy
    6. Spring Boot Validation
    7. MariaDB

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,19 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.allopen") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+}
+
+apply(plugin = "kotlin-jpa")
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 group = "potatowoong"
@@ -26,16 +37,36 @@ configurations {
 
 dependencies {
     // Spring Boot
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     // Log4j2
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
 
     // Kotlin-Logging
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // Spring Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // P6Spy
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.2")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // MariaDB
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
