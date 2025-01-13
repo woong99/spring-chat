253b371ac8d8035a170da42369e11363f799a0fb
Woong99 <dndry1072@naver.com>
2025-01-13 23:17:40 +0900

    feat: QueryDSL 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,14 @@ dependencies {
     // Spring Data JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    // QueryDSL
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
     // Log4j2
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
 
